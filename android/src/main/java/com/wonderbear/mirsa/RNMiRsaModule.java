1f56cbd5c3dcf605ebb400d9575eb6cab6dd3a3e
xiongxiong <ximengwuheng@163.com>
2017-10-27 15:09:45 +0800

    modified

---
+++ android/src/main/java/com/wonderbear/mirsa/RNMiRsaModule.java
@@ -0,0 +1,25 @@
+
+package com.wonderbear.mirsa;
+
+import android.util.Base64;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class RNMiRsaModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNMiRsaModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNMiRsa";
+  }
+
+}
