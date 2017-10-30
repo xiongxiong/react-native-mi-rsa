e58f48a928e9e1b0a21fa47c004b0965c21a4264
xiongxiong <ximengwuheng@163.com>
2017-10-30 14:04:55 +0800

    modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+import React, {Component} from 'react';
 import { NativeModules } from 'react-native';
 
 const { RNMiRsa } = NativeModules;
--- ios/RNMiRsa.m
+++ ios/RNMiRsa.m
@@ -1,13 +1,54 @@
-
 #import "RNMiRsa.h"
+#import "RCTLog.h"
+#import "XRSA.h"
+
+static RNMiRsa *sharedInstance;
+
+@interface RNMiRsa()
+{
+    
+}
+
+@end
 
 @implementation RNMiRsa
 
-- (dispatch_queue_t)methodQueue
+RCT_EXPORT_MODULE();
+
++ (id)allocWithZone:(struct _NSZone *)zone
 {
-    return dispatch_get_main_queue();
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [super allocWithZone:zone];
+    });
+    return sharedInstance;
 }
-RCT_EXPORT_MODULE()
+
++ (instancetype)sharedInstance
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [self new];
+    });
+    return sharedInstance;
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return sharedInstance;
+}
+
+RCT_EXPORT_METHOD(encrypt:(nonnull NSString *)content :(nonnull NSString *)pubKey :(nonnull RCTResponseSenderBlock)callback)
+{
+    RCTLog(@"%@ -- %@", content, pubKey);
+//    NSData *data = [[NSData alloc] initWithBase64EncodedString:pubKey options:NSDataBase64DecodingIgnoreUnknownCharacters];
+//    XRSA *rsa = [[XRSA alloc] initWithData:data];
+//    if (rsa != nil) {
+//        callback(@[[[rsa encryptWithString:content] base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength]]);
+//    } else {
+//        callback(@[@""]);
+//    }
+}
 
 @end
   
